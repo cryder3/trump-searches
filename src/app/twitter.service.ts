a763b47750a239138c531a48c2f3ba8062dedb35
Chad Ryder <cryder3@elon.edu>
2017-05-16 20:32:14 -0400

    work on tiwtter oath

--- src/app/twitter.service.ts
+++ src/app/twitter.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Http, Response} from '@angular/http';
+import {  Headers, Http, Response} from '@angular/http';
 
 import { Tweet } from './tweet/tweet';
 
@@ -10,8 +10,15 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class TwitterService {
 
+private twitterUrl = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=realDonaldTrump&count=20';
+private base64hash = btoa('H0XY1IwEVdoAzZvSRyMmWWYhR:XlsMILpe6XonOWEQ9RE8GuzPLoCMJJWFTXvKJsxWj96oW2T74G');
+
+
+private headers = new Headers({'ContentType':'application/x-www-form-urlencoded;charset=UTF-8'},
+                              {'Authorization': this.base64hash});
+
   constructor(private http: Http) { }
-  private twitterUrl = 'https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=realDonaldTrump&count=20';
+
 
   getTweets(tweets: Tweet[]) {
     this.http.get(this.twitterUrl)
@@ -22,10 +29,17 @@ export class TwitterService {
     return res.json();
   }
 
-  private handleError (error: any) {
-      let errMsg = (error.message) ? error.message :
-                   error.status ? `${error.status} - ${error.statusText}` : 'Server error';
-      console.error(errMsg); // log to console instead
-      return Observable.throw(errMsg);
+  private handleError(error: any): Promise<any> {
+      console.error('An error occurred', error); // for demo purposes only
+      return Promise.reject(error.message || error);
+    }
+
+    private authorizeForTwitter(){
+      const url = `https://api.twitter.com/oauth2/token`;
+      const body = `grant_type=client_credentials`;
+        return this.http.post(url, body, this.headers)
+          .map(res => {return res.json()})
+          .catch(this.handleError);
+
     }
 }
